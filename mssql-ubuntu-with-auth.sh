5729783496ae5ad58ae9c1d1e57eb010a73789cf
adiwong <adiwong@gmail.com>
2019-09-12 21:16:06 -0400

    Create mssql-ubuntu-with-auth.sh

---
+++ mssql-ubuntu-with-auth.sh
@@ -0,0 +1,6 @@
+echo "# mssql-ubuntu" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/adiwong/mssql-ubuntu.git
+git push -u origin master
