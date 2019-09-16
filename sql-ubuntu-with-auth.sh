1a69bd6acedd627bdcd65b5f214151b8aac218e7
adiwong <adiwong@gmail.com>
2019-09-15 20:41:51 -0400

    Update sql-ubuntu-with-auth.sh

--- sql-ubuntu-with-auth.sh
+++ sql-ubuntu-with-auth.sh
@@ -1,6 +1,7 @@
 #retrive username and password
 mssqlAdmin=$1
 mssqlPassword=$2
+MSSQL_PID='evaluation'
 
 # Import the public repository GPG keys
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
@@ -12,23 +13,19 @@ curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /et
 # Install updates
 sudo apt-get update
 sudo apt-get install -y mssql-server
-sudo env ACCEPT_EULA=Y apt-get install apt-get install mssql-tools unixodbc-dev
 
 
-echo "3" | sudo /opt/mssql/bin/mssql-conf set-edition
-echo "$mssqlPassword"; | sudo /opt/mssql/bin/mssql-conf set-sa-password
+sleep 2
+sudo MSSQL_SA_PASSWORD=$mssqlPassword \
+     MSSQL_PID=$MSSQL_PID \
+     /opt/mssql/bin/mssql-conf -n setup accept-eula
 
-sudo systemctl restart mssql-server
+sudo env ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev
+
+sudo systemctl stop mssql-server
 
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 #sudo MSSQL_SA_PASSWORD="$mssqlPassword" /opt/mssql/bin/mssql-conf set-sa-password
 sudo systemctl restart mssql-server
 
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $mssqlPassword -Q "USE MASTER; ALTER LOGIN sa WITH NAME = [$mssqlAdmin];"
-
-# limiting ports
-sudo ufw default deny incoming 
-sudo ufw allow 22
-sudo ufw allow 53
-sudo ufw allow 1433
-echo "y" | sudo ufw enable
